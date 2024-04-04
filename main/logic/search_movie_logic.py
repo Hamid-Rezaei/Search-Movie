c39dbc91282d94a0f7b91552bb6a7de68180423f
hamid <h.rezaei@asax.ir>
2024-04-04 12:57:48 +0330

    feat: logic is patially completed

--- main/logic/search_movie_logic.py
+++ main/logic/search_movie_logic.py
@@ -1,13 +1,27 @@
+from main.dal.rddao.search_movie_rddao import SearchMovieRDDao
 from main.utils.singleton import Singleton
 
 
 class SearchMovieLogic(metaclass=Singleton):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.search_movie_dao = SearchMovieRDDao()
+        self.search_movie_rddao = SearchMovieRDDao()
+        self.search_movie_esdao = SearchMovieESDao()
 
     def search_movie(self, email, audio):
-        ...
+        result = self.search_movie_rddao.get_cached_search_result()
+
+        if not result:
+            result = self.search_movie_esdao()
+            if result:
+                self.search_movie_rddao.cache_search_result()
+            else:
+             ... #TODO: call api
+
+        result = self.search_movie_rddao.get_cached_search_result()
+        movie_info = None
+
+        return movie_info
 
 
 
